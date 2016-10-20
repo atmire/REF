2adf477258add6fc97acaa0deb61e01d598928aa
Philip Vissenaekens <philipvissenaekens@atmire.com>
2016-10-20 15:19:09 +0200

    37020: Mirage 2 exception step

--- dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/ref-exceptions.js
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/ref-exceptions.js
@@ -1,16 +1,22 @@
 (function ($) {
-    $('.subdivision').change(function () {
-        var value = $(this).val();
-        $('.helpText').addClass("hidden");
-        $('#aspect_submission_StepTransformer_p_' + value).removeClass("hidden");
-    });
-    $('input[name="exception-options"]').click(function () {
-        var value = $(this).val();
-        $('.specificExceptions').addClass("hidden");
-        $("#aspect_submission_StepTransformer_div_" + value).removeClass("hidden");
-        $('.helpText').addClass("hidden");
-        var helpText = $('select[name="' + value + '-dropdown"] option:selected').val();
-        $('#aspect_submission_StepTransformer_p_' + helpText).removeClass("hidden");
+
+    $(document).ready(function() {
+
+        $('.subdivision').change(function () {
+            var value = $(this).val();
+            $('.helpText').addClass("hidden");
+            $('#aspect_submission_StepTransformer_p_' + value).removeClass("hidden");
+        });
+        $('input[name="exception-options"]').click(function () {
+            var value = $(this).val();
+            $('.specificExceptions').addClass("hidden");
+            $("#aspect_submission_StepTransformer_div_" + value).removeClass("hidden");
+            $('.helpText').addClass("hidden");
+            var helpText = $('select[name="' + value + '-dropdown"] option:selected').val();
+            $('#aspect_submission_StepTransformer_p_' + helpText).removeClass("hidden");
+
+        });
 
     });
+
 })(jQuery);
